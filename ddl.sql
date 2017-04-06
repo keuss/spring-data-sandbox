3e93a09870b988651075b217bbbdb17ecaea541f
Guillaume GALLOIS <guillaume.gallois@gmail.com>
2017-04-06 22:15:16 +0200

    add users DDL

--- ddl.sql
+++ ddl.sql
@@ -12,3 +12,19 @@ WITH (
 );
 ALTER TABLE public.thirdparty
   OWNER TO "sandbox-user";
+  
+CREATE TABLE public.users
+(
+  id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass), -- user's internal id
+  name character(20), -- user's name
+  email character(40), -- user's mail
+  CONSTRAINT id_pk PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.users
+  OWNER TO "sandbox-user";
+COMMENT ON COLUMN public.users.id IS 'user''s internal id';
+COMMENT ON COLUMN public.users.name IS 'user''s name';
+COMMENT ON COLUMN public.users.email IS 'user''s mail';
